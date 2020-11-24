adf94970814ee6e1af8a78b39cc144c9bb89cc61
Ola Assem <o.assem@gmail.com>
2020-11-24 15:16:42 +0200

    hdr illustration elastic anime

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 gsap.from('.background-pattern', {
   opacity: 0,
-  duration: 25,
-  y: -50,
-  ease: Elastic.easeOut.config(1, 0.3), y: 300
+  duration: 1,
+  y: 80,
 });
 
 gsap.from('.hdr-stagger', {
   opacity: 0,
-  duration: 2,
+  duration: 1,
   y: -50,
   stagger: 0.6
+});
+
+gsap.from('.img-woman', {
+  opacity: 0,
+  duration: 5,
+  y: -50,
+  ease: Elastic.easeOut.config(1, 0.3), y: -80
+});
+
+gsap.from('.img-flowers', {
+  opacity: 0,
+  duration: 30,
+  y: 50,
+  ease: Elastic.easeOut.config(1, 0.3), y: 100
 });
 
