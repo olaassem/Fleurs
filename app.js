2fbaf34c71f004337e26a2a33c6dea5f98c35866
Ola Assem <o.assem@gmail.com>
2020-11-24 15:05:29 +0200

    stagger hdr text & dropshadow

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 gsap.from('.background-pattern', {
   opacity: 0,
-  duration: 8,
+  duration: 25,
   y: -50,
-  ease: Elastic.easeOut.config(1, 0.3), y: 200
-})
+  ease: Elastic.easeOut.config(1, 0.3), y: 300
+});
+
+gsap.from('.hdr-stagger', {
+  opacity: 0,
+  duration: 2,
+  y: -50,
+  stagger: 0.6
+});
 
