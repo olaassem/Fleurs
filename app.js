889e01afbae77dcaabab4ccb058ffa70eec5e762
Ola Assem <ola.assem@viget.com>
2020-11-29 11:35:10 +0200

    scrolltrigger pluggin border animation

--- app.js
+++ app.js
@@ -7,4 +7,17 @@ hdrTimeline.from('.background-pattern',{ duration: 1, y: 80 })
   .from('.hdr-stagger', { duration: 1, y: -50, stagger: 0.4 }, "-=9");
 
 
+//Border Animation
+let borderTimeline = gsap.timeline({
+  defaults: {
+    opacity: 0
+  },
+  scrollTrigger: {
+    trigger: '.flower-border',
+    start: "center bottom"
+  }
+});
 
+borderTimeline.from('.border-anime-1', { duration: 1.5, y: 40 })
+  .from('.border-anime-2', { duration: 2, y: 40}, "-=1")
+  .from('.border-anime-3', { duration: 1.5, y: 40}, "-=1.5");
